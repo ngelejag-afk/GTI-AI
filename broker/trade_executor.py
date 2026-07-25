4ed6f97367c2b0156d82518fa3de45a32e2568e8
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 00:35:01 +0300

    Implement TradeExecutor class for order execution

--- broker/trade_executor.py
+++ broker/trade_executor.py
@@ -1,1 +1,35 @@
+"""
+GTI AI
+Trade Executor
+Version 1.0
+"""
 
+from broker.broker import Broker
+from broker.order import Order
+from broker.order_engine import OrderEngine
+
+
+class TradeExecutor:
+    """
+    Executes trading orders through a broker.
+    """
+
+    def __init__(
+        self,
+        broker: Broker,
+        order_engine: OrderEngine,
+    ) -> None:
+        self.broker = broker
+        self.order_engine = order_engine
+
+    def execute(self, order: Order) -> bool:
+        """
+        Execute a trading order.
+        """
+
+        if not self.broker.is_connected():
+            return False
+
+        self.order_engine.place(order)
+
+        return True
