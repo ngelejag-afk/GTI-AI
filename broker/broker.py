1afb6e79be7ca432697431cdd100cbe877901e7a
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 00:12:34 +0300

    Create Broker interface for broker integrations
    Added abstract base class for broker integrations with methods for connecting, disconnecting, and checking connection status.

--- broker/broker.py
+++ broker/broker.py
@@ -1,1 +1,34 @@
+"""
+GTI AI
+Broker Interface
+Version 1.0
+"""
 
+from abc import ABC, abstractmethod
+
+
+class Broker(ABC):
+    """
+    Base interface for broker integrations.
+    """
+
+    @abstractmethod
+    def connect(self) -> bool:
+        """
+        Connect to the broker.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def disconnect(self) -> None:
+        """
+        Disconnect from the broker.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_connected(self) -> bool:
+        """
+        Return connection status.
+        """
+        raise NotImplementedError
