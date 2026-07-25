17d41497a6aa361c1f5d18c67dee05ad97ba39a3
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 00:45:02 +0300

    Add Portfolio class with trading account attributes

--- broker/portfolio.py
+++ broker/portfolio.py
@@ -1,1 +1,20 @@
+"""
+GTI AI
+Portfolio Model
+Version 1.0
+"""
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Portfolio:
+    """
+    Represents a trading account.
+    """
+
+    balance: float
+    equity: float
+    margin: float
+    free_margin: float
+    profit: float
