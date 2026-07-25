6ba48677eaa9c7256127795976c10c5d393399f4
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 00:19:13 +0300

    Add DemoBroker class with connection functionality
    Implement a demo broker class with connection methods.

--- broker/demo_broker.py
+++ broker/demo_broker.py
@@ -1,1 +1,38 @@
+"""
+GTI AI
+Demo Broker
+Version 1.0
+"""
 
+from broker.broker import Broker
+
+
+class DemoBroker(Broker):
+    """
+    Demo implementation of a broker.
+    """
+
+    def __init__(self) -> None:
+        self._connected = False
+
+    def connect(self) -> bool:
+        """
+        Connect to the demo broker.
+        """
+
+        self._connected = True
+        return True
+
+    def disconnect(self) -> None:
+        """
+        Disconnect from the demo broker.
+        """
+
+        self._connected = False
+
+    def is_connected(self) -> bool:
+        """
+        Return connection status.
+        """
+
+        return self._connected
