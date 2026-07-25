5bf0f6e2bd741452d31e287fa915177a0d1bd4a9
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 00:42:35 +0300

    Implement TradeManager class for trade management
    Added TradeManager class to manage trading rules for open positions.

--- broker/trade_manager.py
+++ broker/trade_manager.py
@@ -1,1 +1,30 @@
+"""
+GTI AI
+Trade Manager
+Version 1.0
+"""
 
+from broker.position_engine import PositionEngine
+
+
+class TradeManager:
+    """
+    Manages trading rules for open positions.
+    """
+
+    def __init__(self, positions: PositionEngine) -> None:
+        self.positions = positions
+
+    def can_open_trade(self) -> bool:
+        """
+        Return True if a new trade can be opened.
+        """
+
+        return self.positions.count() == 0
+
+    def has_open_trade(self) -> bool:
+        """
+        Return True if there is an open trade.
+        """
+
+        return self.positions.count() > 0
