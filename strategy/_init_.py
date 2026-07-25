f3d1c337ae993901705e3085fc6805c60025e126
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 01:19:04 +0300

    Initialize strategy package with docstring and exports
    Add module docstring and define public API.

--- strategy/_init_.py
+++ strategy/_init_.py
@@ -1,1 +1,15 @@
+"""
+GTI AI
+Strategy Package
+Version 1.0
+"""
 
+from .base_strategy import BaseStrategy
+from .trend_strategy import TrendStrategy
+from .strategy_engine import StrategyEngine
+
+__all__ = [
+    "BaseStrategy",
+    "TrendStrategy",
+    "StrategyEngine",
+]
